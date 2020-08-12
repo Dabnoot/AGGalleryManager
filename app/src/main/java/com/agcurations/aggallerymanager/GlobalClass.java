53e408774e06f1cf293e25222aa01d6b4ae8bca9
Will Chapman <William.Chapman970@gmail.com>
2020-08-11 21:14:40 -0400

    Menu working. Color scheme ok. Obfuscation of first activity ok.

---
+++ app/src/main/java/com/agcurations/aggallerymanager/GlobalClass.java
@@ -0,0 +1,166 @@
+package com.agcurations.aggallerymanager;
+
+
+
+import android.app.Application;
+
+import java.io.File;
+import java.util.TreeMap;
+
+public class GlobalClass extends Application {
+    //GlobalClass built using this guide:
+    //https://androidexample.com/Global_Variable_Or_Application_Context_Variable_-_Android_Example/index.php?view=article_discription&aid=114&aaid=136
+
+    //Global Variables:
+    private File gvfCatalogComicsFolder;
+    private File gvfLogsFolder;
+    private File gvfCatalogContentsFile; //CatalogContentsFile record fields: ComicID, ComicName, MaxPageID, MissingPages, ComicSize (in kB)
+    TreeMap<Integer, String[]> gvtmCatalogComicList;
+
+    public static final int COMIC_ID_INDEX = 0;                 //Comic ID
+    public static final int COMIC_NAME_INDEX = 1;               //Comic Name
+    public static final int COMIC_FILE_COUNT_INDEX = 2;         //Files included with the comic
+    public static final int COMIC_MAX_PAGE_ID_INDEX = 3;        //Max page ID extracted from file names
+    public static final int COMIC_MISSING_PAGES_INDEX = 4;      //String of comma-delimited missing page numbers
+    public static final int COMIC_SIZE_KB_INDEX = 5;            //Total size of all files in the comic
+    public static final int COMIC_FOLDER_NAME_INDEX = 6;        //Name of the folder holding the comic pages
+    public static final int COMIC_THUMBNAIL_FILE_INDEX = 7;     //Name of the file used as the thumbnail for the comic
+    //public static final int COMIC_PARODIES_INDEX = 8;
+    //public static final int COMIC_CHARACTERS_INDEX = 9;
+    //public static final int COMIC_TAGS_INDEX = 10;              //Tags given to the comic
+    //public static final int COMIC_ARTISTS_INDEX = 11;
+    //public static final int COMIC_GROUPS_INDEX = 12;
+    //public static final int COMIC_LANGUAGES_INDEX = 13;         //Language(s) found in the comic
+    //public static final int COMIC_CATEGORIES_INDEX = 14;
+    //public static final int COMIC_PAGES_INDEX = 15;             //Total number of pages as defined at the comic source
+
+    public static final String[] ComicRecordFields = new String[]{
+            "COMIC_ID",
+            "COMIC_NAME",
+            "FILE_COUNT",
+            "MAX_PAGE_ID",
+            "MISSING_PAGES",
+            "COMIC_SIZE_KB",
+            "FOLDER_NAME",
+            "THUMBNAIL_FILE",
+            "PARODIES",
+            "CHARACTERS",
+            "TAGS",
+            "ARTISTS",
+            "GROUPS",
+            "LANGUAGES",
+            "CATEGORIES",
+            "PAGES"};
+
+
+
+
+    public File getCatalogComicsFolder() {
+        return gvfCatalogComicsFolder;
+    }
+    public void setCatalogComicsFolder(File fCatalogComicsFolder){
+        gvfCatalogComicsFolder = fCatalogComicsFolder;
+    }
+
+    public File getLogsFolder() {
+        return gvfLogsFolder;
+    }
+    public void setLogsFolder(File fLogsFolder){
+        gvfLogsFolder = fLogsFolder;
+    }
+
+    public File getCatalogContentsFile() {
+        return gvfCatalogContentsFile;
+    }
+    public void setCatalogContentsFile(File fCatalogContentsFile){
+        gvfCatalogContentsFile = fCatalogContentsFile;
+    }
+
+    public TreeMap<Integer, String[]> getCatalogComicList() {
+        return gvtmCatalogComicList;
+    }
+    public void setCatalogComicList(TreeMap<Integer, String[]> tmCatalogComicList){
+        gvtmCatalogComicList = tmCatalogComicList;
+    }
+
+
+    //Begin Obfuscation section:
+
+    public boolean ObfuscationOn = true;
+    public int iObfuscationIndex;
+    //public int OBFUSCATION_SUBJECT_VIDEOGAMES = 0;
+    public int OBFUSCATION_SUBJECT_QUALITY = 1;
+    public int iObfuscationSubjectSelection = OBFUSCATION_SUBJECT_QUALITY;
+
+    int[][] iImageList = new int[][]{
+            {
+                    R.drawable.ovg_death_stranding,
+                    R.drawable.ovg_doom_eternal,
+                    R.drawable.ovg_fallout_new_vegas,
+                    R.drawable.ovg_horizon_zero_dawn,
+                    R.drawable.ovg_resident_evil_2
+            },
+            {
+                    R.drawable.oq_cost_of_poor_quality,
+                    R.drawable.oq_five_why,
+                    R.drawable.oq_ishikawa_diagram,
+                    R.drawable.oq_ishikawa_diagram2,
+                    R.drawable.oq_kanban_method_principles,
+                    R.drawable.oq_kanban_method,
+                    R.drawable.oq_mccalls_quality_factors
+            }
+    };
+    String[][] sObfuscationCategoryNames = new String[][]{
+            {
+                    "Top 10 PS4 Games 2015",
+                    "Top 10 PS4 Games 2016",
+                    "Top 10 PS4 Games 2017",
+                    "Top 10 PS4 Games 2018",
+                    "Top 10 PS4 Games 2019"
+            },
+            {
+                    "Cost of Poor Quality",
+                    "Five Why Diagram",
+                    "Ishikawa Diagram 1",
+                    "Ishikawa Diagram 2",
+                    "Kanban Method Principles",
+                    "Kanban Method",
+                    "McCall's Quality Factors"
+            }
+    };
+    String[] sObfuscatedProgramNames = new String[]{
+            "Top Titles",
+            "Quality Operations"
+    };
+    String sNonObfustatedProgramName = "Comic Catalog";
+
+    public int getObfuscationImageCount(){
+        return iImageList[iObfuscationSubjectSelection].length;
+    }
+
+    public int getObfuscationImage(int index) {
+        if(index >= iImageList[iObfuscationSubjectSelection].length - 1){
+            index = 0;
+        }
+        return iImageList[iObfuscationSubjectSelection][index];
+    }
+
+    public String getObfuscationCategoryName(){
+        return sObfuscationCategoryNames[iObfuscationSubjectSelection][iObfuscationIndex];
+    }
+
+    public String getObfuscatedProgramName() {
+        return sObfuscatedProgramNames[iObfuscationSubjectSelection];
+    }
+
+    public String getNonObfuscatedProgramName(){
+        return sNonObfustatedProgramName;
+    }
+
+    //End obfuscation section.
+
+
+
+
+}
+
--- app/src/main/java/com/agcurations/aggallerymanager/MainActivity.java
+++ app/src/main/java/com/agcurations/aggallerymanager/MainActivity.java
@@ -2,15 +2,122 @@ package com.agcurations.aggallerymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Global Variables:
+    GlobalClass globalClass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Return theme away from startup_screen
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Calling Application class (see application tag in AndroidManifest.xml)
+        globalClass = (GlobalClass) getApplicationContext();
+
     }
+
+    //=====================================================================================
+    //===== Menu Code =================================================================
+    //=====================================================================================
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Display a message showing the name of the item selected.
+        //Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+
+            case R.id.menu_FlipView:
+                FlipObfuscation();
+                return true;
+
+            case R.id.menu_About:
+                //Intent intentAbout = new Intent(this, AboutActivity.class);
+                //startActivity(intentAbout);
+                return true;
+
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    //=====================================================================================
+    //===== Obfuscation Code =================================================================
+    //=====================================================================================
+
+    public void FlipObfuscation() {
+        //This routine primarily accessed via the toggle option on the menu.
+        globalClass.ObfuscationOn = !globalClass.ObfuscationOn;
+        if(globalClass.ObfuscationOn) {
+            //Obfuscate data:
+            Obfuscate();
+        } else {
+            //Remove obfuscation:
+            ImageView ivVideos = findViewById(R.id.imageView_Video);
+            ImageView ivPictures = findViewById(R.id.imageView_Pictures);
+            ImageView ivComics = findViewById(R.id.imageView_Comics);
+
+            ivVideos.setImageResource(R.drawable.nob_videos);
+            ivPictures.setImageResource(R.drawable.nob_pictures);
+            ivComics.setImageResource(R.drawable.nob_comics);
+
+            TextView tvVideos = findViewById(R.id.textView_Videos);
+            TextView tvPictures = findViewById(R.id.textView_Pictures);
+            TextView tvComics = findViewById(R.id.textView_Comics);
+
+            tvVideos.setText(R.string.videos);
+            tvPictures.setText(R.string.pictures);
+            tvComics.setText(R.string.comics);
+       }
+
+    }
+
+    public void Obfuscate() {
+        //This routine is separate because it can be activated
+        // by either a long-press or the toggle option on the menu.
+        if(!globalClass.ObfuscationOn) {
+            globalClass.ObfuscationOn = true;
+        }
+
+        ImageView ivVideos = findViewById(R.id.imageView_Video);
+        ImageView ivPictures = findViewById(R.id.imageView_Pictures);
+        ImageView ivComics = findViewById(R.id.imageView_Comics);
+
+        ivVideos.setImageResource(R.drawable.ob_productivity_tools);
+        ivPictures.setImageResource(R.drawable.ob_data_management);
+        ivComics.setImageResource(R.drawable.ob_quality_analysis);
+
+        TextView tvVideos = findViewById(R.id.textView_Videos);
+        TextView tvPictures = findViewById(R.id.textView_Pictures);
+        TextView tvComics = findViewById(R.id.textView_Comics);
+
+        tvVideos.setText(R.string.productivity_tools);
+        tvPictures.setText(R.string.data_management);
+        tvComics.setText(R.string.quality_analysis);
+
+
+    }
+
 
 
 
