8f0ecf62671d02739339a3b3c821ec1d04be580c
Will Chapman <William.Chapman970@gmail.com>
2020-12-30 17:15:55 -0500

    Modified popup so that when the keyboard is shown the view moves up above the keyboard.

--- app/src/main/java/com/agcurations/aggallerymanager/Activity_PinCodePopup.java
+++ app/src/main/java/com/agcurations/aggallerymanager/Activity_PinCodePopup.java
@@ -3,11 +3,17 @@ package com.agcurations.aggallerymanager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class Activity_PinCodePopup extends Activity {
 
@@ -20,6 +26,10 @@ public class Activity_PinCodePopup extends Activity {
     public static final int PIN_CODE_MATCH = -1;
     public static final int PIN_CODE_NO_MATCH = 0;
 
+    int giKeyboardAdjusted_y;
+    boolean gbLPKeyboardVisibility = false;
+    int giKbTick = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +40,49 @@ public class Activity_PinCodePopup extends Activity {
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         int width = dm.widthPixels;
-        int height = dm.heightPixels;
+        final int height = dm.heightPixels;
 
         float fSize = 0.35f;
 
         getWindow().setLayout((int)(width * fSize), (int)(height * fSize));
+
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE;
+        getWindow().setAttributes(lp);
+
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        RelativeLayout relativeLayout_PinCode = findViewById(R.id.relativeLayout_PinCode);
+
+        giKeyboardAdjusted_y = getActionBarHeight() + getStatusBarHeight() + 10;
+
+        ViewCompat.setOnApplyWindowInsetsListener(relativeLayout_PinCode, new androidx.core.view.OnApplyWindowInsetsListener(){
+
+            @Override
+            public WindowInsetsCompat onApplyWindowInsets(View v, WindowInsetsCompat insets) {
+                WindowManager.LayoutParams lp = getWindow().getAttributes();
+                boolean bCurrentKeyboardIsVisible = insets.isVisible(WindowInsetsCompat.Type.ime());
+
+                if(bCurrentKeyboardIsVisible != gbLPKeyboardVisibility && giKbTick == 0) {
+                    if (bCurrentKeyboardIsVisible) {
+                        giKbTick = 1;
+                        int iPinCodePopupHeight = v.getHeight();
+                        int iHeightMarginTop = (height - iPinCodePopupHeight) / 2;
+                        lp.y = giKeyboardAdjusted_y - iHeightMarginTop;
+                    } else {
+                        lp.y = 0;
+                    }
+                    getWindow().setAttributes(lp);
+                    gbLPKeyboardVisibility = bCurrentKeyboardIsVisible;
+                } else {
+                    giKbTick = 0;
+                }
+
+                return insets;
+            }
+
+        });
+
 
     }
 
@@ -59,5 +107,39 @@ public class Activity_PinCodePopup extends Activity {
         finish();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+/*    public int getNavigationBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }*/
+
+    public int getActionBarHeight() {
+        int result = 0;
+        // Calculate ActionBar height
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true))
+        {
+            result = TypedValue.complexToDimensionPixelSize(tv.data,getResources().getDisplayMetrics());
+        }
+        return result;
+    }
 
 }
