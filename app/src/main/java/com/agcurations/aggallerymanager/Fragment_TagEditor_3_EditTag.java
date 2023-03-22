65eea4b4f27a3290ff49d885d8fac78a6da45741
Dabnoot <William.Chapman970@gmail.com>
2023-03-21 21:57:05 -0400

    Added code to Edit Text fragment to display tag description. Significant code mods to allow more robust tag selection in preparation for true implementation of the tag edit feature. Tag selection for edit will not show tags which are above a user's maturity rating or tags that are private to a particular user.

--- app/src/main/java/com/agcurations/aggallerymanager/Fragment_TagEditor_3_EditTag.java
+++ app/src/main/java/com/agcurations/aggallerymanager/Fragment_TagEditor_3_EditTag.java
@@ -1,7 +1,10 @@
 package com.agcurations.aggallerymanager;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -9,9 +12,9 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,6 +22,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +33,10 @@ public class Fragment_TagEditor_3_EditTag extends Fragment {
 
     private GlobalClass globalClass;
     private ViewModel_TagEditor viewModelTagEditor;
+
+    private TextView gTextView_TagID;
+    private EditText gEditText_TagText;
+    private EditText gEditText_TagDescription;
 
     public Fragment_TagEditor_3_EditTag() {
         // Required empty public constructor
@@ -102,44 +110,63 @@ public class Fragment_TagEditor_3_EditTag extends Fragment {
         if (getView() == null) {
             return;
         }
-        final ListView listView_TagViewer = getView().findViewById(R.id.listView_TagViewer);
 
-        ArrayList<String> alsTags = new ArrayList<>();
-        for(Map.Entry<Integer, ItemClass_Tag> entry : globalClass.gtmCatalogTagReferenceLists.get(viewModelTagEditor.iTagEditorMediaCategory).entrySet()){
-            String sItemText = entry.getValue().sTagText + " (ID:" + entry.getValue().iTagID + ")";
-            alsTags.add(sItemText);
-        }
-        //Alphabetize the tags:
-        alsTags.sort(String.CASE_INSENSITIVE_ORDER);
+        gTextView_TagID = getView().findViewById(R.id.textView_TagID);
+        gEditText_TagText = getView().findViewById(R.id.editText_TagText);
+        gEditText_TagDescription = getView().findViewById(R.id.editText_TagDescription);
+        gTextView_TagID.setText("");
+        gEditText_TagText.setText("");
+        gEditText_TagDescription.setText("");
 
-        String[] sTemp = new String[alsTags.size()];
-        sTemp = alsTags.toArray(sTemp);
-        if (getActivity() == null) {
-            return;
-        }
-        ArrayAdapter<String> aasTags = new ArrayAdapter<>(getActivity(), R.layout.listview_tageditor_tagtext, sTemp);
-        listView_TagViewer.setAdapter(aasTags);
-
-        final EditText editText_TagText = getView().findViewById(R.id.editText_UserName);
-        final TextView textView_TagID = getView().findViewById(R.id.textView_TagID);
-        editText_TagText.setText("");
-        textView_TagID.setText("");
-
-        listView_TagViewer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String sItemText = (String)adapterView.getItemAtPosition(position);
-                String[] sFields = sItemText.split(" \\(ID:");
-                String sTagName = sFields[0];
-                String sTagID = sFields[1].substring(0, sFields[1].length()-1);
-                textView_TagID.setText(sTagID);
-                editText_TagText.setText(sTagName);
-                Button button_Apply = getView().findViewById(R.id.button_Apply);
-                if(button_Apply != null){
-                    button_Apply.setEnabled(true);
+        TreeMap<String, ItemClass_Tag> tmTags = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        boolean bIsTagToBeRestricted = false;
+        for (Map.Entry<Integer, ItemClass_Tag> entry : globalClass.gtmCatalogTagReferenceLists.get(viewModelTagEditor.iTagEditorMediaCategory).entrySet()) {
+            //if (entry.getValue().bIsRestricted) {
+            if(globalClass.gicuCurrentUser != null) {
+                if (globalClass.gicuCurrentUser.iMaturityLevel < entry.getValue().iTagAgeRating) {
+                    bIsTagToBeRestricted = true;
+                }
+            } else {
+                //If no user is selected or current user is somehow null, follow guidelines for
+                //  default user maturity rating.
+                if (entry.getValue().iTagAgeRating <= globalClass.giDefaultUserMaturityRating) {
+                    bIsTagToBeRestricted = true;
                 }
             }
-        });
+            if(!bIsTagToBeRestricted){
+                if(entry.getValue().alsTagApprovedUsers != null){
+                    if(entry.getValue().alsTagApprovedUsers.size() > 0){
+                        bIsTagToBeRestricted = true;
+                        for(String sApprovedUser: entry.getValue().alsTagApprovedUsers){
+                            if (globalClass.gicuCurrentUser.sUserName.equals(sApprovedUser)){
+                                bIsTagToBeRestricted = false;
+                                break;
+                            }
+
+                        }
+                    }
+                }
+            }
+            if (!bIsTagToBeRestricted) {
+                String sTagTextForSort = entry.getValue().sTagText + entry.getValue().iTagID;
+                tmTags.put(sTagTextForSort, entry.getValue());
+            }
+
+        }
+
+        ArrayList<ItemClass_Tag> alict_TagsListTags = new ArrayList<>();
+
+        for(Map.Entry<String, ItemClass_Tag> entry : tmTags.entrySet()){
+            alict_TagsListTags.add(entry.getValue());
+        }
+
+        if(getActivity() != null) {
+            ListViewTagsAdapter listViewTagsAdapter = new ListViewTagsAdapter(getActivity().getApplicationContext(), alict_TagsListTags);
+            ListView listView_TagViewer = getView().findViewById(R.id.listView_TagViewer);
+            listView_TagViewer.setAdapter(listViewTagsAdapter);
+            listView_TagViewer.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        }
+
     }
 
 
@@ -177,6 +204,91 @@ public class Fragment_TagEditor_3_EditTag extends Fragment {
     }
 
 
+
+
+
+    public class ListViewTagsAdapter extends ArrayAdapter<ItemClass_Tag> {
+
+        ArrayList<ItemClass_Tag> alictTagItems; //Contains all tag items passed to the listviewTagsAdapter.
+
+        public ListViewTagsAdapter(Context context, ArrayList<ItemClass_Tag> tagItems) {
+            super(context, 0, tagItems);
+            alictTagItems = tagItems;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View v, @NonNull ViewGroup parent) {
+            // Get the data item for this position
+
+            final ItemClass_Tag tagItem = alictTagItems.get(position);
+
+            if(tagItem == null){
+                return v;
+            }
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (v == null) {
+                v = LayoutInflater.from(getContext()).inflate(R.layout.listview_tag_item_select_tags_fragment, parent, false);
+            }
+            // Lookup view for data population
+            final CheckedTextView checkedTextView_TagText = v.findViewById(R.id.checkedTextView_TagText);
+            checkedTextView_TagText.setText(tagItem.sTagText);
+
+            //Set the selection state (needed as views are recycled).
+            if(getActivity() != null) {
+                if (tagItem.bIsChecked) {
+                    checkedTextView_TagText.setBackgroundColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorFragmentBackgroundHighlight2));
+                } else {
+                    checkedTextView_TagText.setBackgroundColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorUnfilledUnselected));
+                }
+                checkedTextView_TagText.setTextColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorTextColor));
+            }
+
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Handle changing the checked state:
+                    tagItem.bIsChecked = !tagItem.bIsChecked;
+                    boolean bUpdateOtherItemsViews = false;
+                    if(tagItem.bIsChecked){
+                        checkedTextView_TagText.setBackgroundColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorFragmentBackgroundHighlight2));
+                        checkedTextView_TagText.setTextColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorTextColor));
+                        String sTagID = "" + tagItem.iTagID;
+                        gTextView_TagID.setText(sTagID);
+                        gEditText_TagText.setText(tagItem.sTagText);
+                        gEditText_TagDescription.setText(tagItem.sTagDescription);
+                        //Go through and uncheck anything but this tag:
+                        for(ItemClass_Tag ict: alictTagItems){
+                            if(ict.bIsChecked && !ict.iTagID.equals(tagItem.iTagID)){
+                                ict.bIsChecked = false;
+                                bUpdateOtherItemsViews = true;
+                            }
+                        }
+                    } else {
+                        checkedTextView_TagText.setBackgroundColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorUnfilledUnselected));
+                        checkedTextView_TagText.setTextColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorTextColor));
+                    }
+                    if(bUpdateOtherItemsViews){
+                        notifyDataSetChanged();
+                    }
+
+
+                }
+            });
+
+            // Return the completed view to render on screen
+            return v;
+        }
+
+
+        @Override
+        public int getCount() {
+            return super.getCount();
+        }
+    }
+
+
+
     public void setupUI(View view) {
         //https://stackoverflow.com/questions/4165414/how-to-hide-soft-keyboard-on-android-after-clicking-outside-edittext/19828165
         // Set up touch listener for non-text box views to hide keyboard.
@@ -199,8 +311,5 @@ public class Fragment_TagEditor_3_EditTag extends Fragment {
             }
         }
     }
-
-
-
 
 }
