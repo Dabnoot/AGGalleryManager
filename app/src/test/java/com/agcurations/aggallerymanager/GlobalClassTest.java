ac6c48d5e1678892559b143d0c92978a207930b0
Dabnoot <William.Chapman970@gmail.com>
2023-07-28 16:35:06 -0400

    Started "unit testing" for code.

--- app/src/test/java/com/agcurations/aggallerymanager/GlobalClassTest.java
+++ app/src/test/java/com/agcurations/aggallerymanager/GlobalClassTest.java
@@ -3,22 +3,25 @@ package com.agcurations.aggallerymanager;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+import static org.junit.Assert.assertEquals;
+
 public class GlobalClassTest {
 
     @Test
-    public void cleanHTMLCodedCharacters() {
+    public void SplitFileNameIntoBaseAndExtension() {
 
-        String sTestData = "content://com.android.externalstorage.documents/tree/3031-3461%3AArchive/document/3031-3461%3AArchive%2FAGGalleryManager%2FComics%2F319549%2Frtrktzhppzdkfgkq4gb5arb56i";
-        String sExpectedResult = "content://com.android.externalstorage.documents/tree/3031-3461:Archive/document/3031-3461:Archive/AGGalleryManager/Comics/319549/rtrktzhppzdkfgkq4gb5arb56i";
+        String[] sFileNameTests = {"hello.jpeg",
+                                   "trythis.png"};
+        String[][] sBaseAndExtensionExpected = {
+                {"hello", "jpeg"},
+                {"trythis", "png"}};
+        String[] sBaseAndExtensionResult;
 
-        String sResult = "";
-        sResult = GlobalClass.cleanHTMLCodedCharacters(sTestData);
-
-        assertEquals(sResult, sExpectedResult);
+        for(String sFileNameTest: sFileNameTests){
+            sBaseAndExtensionResult = GlobalClass.SplitFileNameIntoBaseAndExtension(sFileNameTest);
+            assertEquals(sBaseAndExtensionResult.length, 2);
+        }
 
     }
 }
