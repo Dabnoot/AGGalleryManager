997e738f2d128df55d30956fc2395a3fe227876e
Dabnoot <William.Chapman970@gmail.com>
2023-07-28 16:22:06 -0400

    Started "unit testing" for code.

---
+++ app/src/androidTest/java/com/agcurations/aggallerymanager/GlobalClassTest.java
@@ -0,0 +1,22 @@
+package com.agcurations.aggallerymanager;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GlobalClassTest {
+
+    @Test
+    public void cleanHTMLCodedCharacters() {
+
+        String sTestData = "content://com.android.externalstorage.documents/tree/3031-3461%3AArchive/document/3031-3461%3AArchive%2FAGGalleryManager%2FComics%2F319549%2Frtrktzhppzdkfgkq4gb5arb56i";
+        String sExpectedResult = "content://com.android.externalstorage.documents/tree/3031-3461:Archive/document/3031-3461:Archive/AGGalleryManager/Comics/319549/rtrktzhppzdkfgkq4gb5arb56i";
+
+        String sResult = "";
+        sResult = GlobalClass.cleanHTMLCodedCharacters(sTestData);
+
+        assertEquals(sResult, sExpectedResult);
+
+    }
+
+}
