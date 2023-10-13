a03b7d84c7736a210eb34f1591f279228a268bdf
Dabnoot <William.Chapman970@gmail.com>
2023-10-13 10:55:16 -0400

    Added logic to set focus to the pin EditText if the selected user requires a pin for access.

--- app/src/main/java/com/agcurations/aggallerymanager/Activity_UserSelection.java
+++ app/src/main/java/com/agcurations/aggallerymanager/Activity_UserSelection.java
@@ -12,7 +12,6 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -47,71 +46,68 @@ public class Activity_UserSelection extends AppCompatActivity {
                 getApplicationContext(), R.layout.listview_useritem, alicuAllUserPool);
         //adapterUserList.gbSimplifiedView = true;  //Hides the maturity rating and admin status
         listView_UserList.setAdapter(adapterUserList);
-        listView_UserList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                final ItemClass_User icu = (ItemClass_User) parent.getItemAtPosition(position);
-                final String sWelcomeMessage = "Welcome, " + icu.sUserName + ".";
-                if(!icu.sPin.equals("")){
-                    //If this user record requires a pin to log-in, display the pin code popup:
+        //End if ListView.onItemClick().
+        listView_UserList.setOnItemClickListener((parent, view, position, id) -> {
+            final ItemClass_User icu = (ItemClass_User) parent.getItemAtPosition(position);
+            final String sWelcomeMessage = "Welcome, " + icu.sUserName + ".";
+            if(!icu.sPin.equals("")){
+                //If this user record requires a pin to log-in, display the pin code popup:
 
-                    //Configure the AlertDialog that will gather the pin code if necessary to begin a particular behavior:
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(parent.getContext(), R.style.AlertDialogCustomStyle);
+                //Configure the AlertDialog that will gather the pin code if necessary to begin a particular behavior:
+                final AlertDialog.Builder builder = new AlertDialog.Builder(parent.getContext(), R.style.AlertDialogCustomStyle);
 
-                    // set the custom layout
-                    LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-                    final View customLayout = inflater.inflate(R.layout.dialog_layout_pin_code, null);
-                    builder.setView(customLayout);
+                // set the custom layout
+                LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                final View customLayout = inflater.inflate(R.layout.dialog_layout_pin_code, null);
+                builder.setView(customLayout);
 
-                    final AlertDialog adConfirmationDialog = builder.create();
+                final AlertDialog adConfirmationDialog = builder.create();
 
-                    //Code action for the Cancel button:
-                    Button button_PinCodeCancel = customLayout.findViewById(R.id.button_PinCodeCancel);
-                    button_PinCodeCancel.setOnClickListener(view1 -> adConfirmationDialog.dismiss());
+                //Code action for the Cancel button:
+                Button button_PinCodeCancel = customLayout.findViewById(R.id.button_PinCodeCancel);
+                button_PinCodeCancel.setOnClickListener(view1 -> adConfirmationDialog.dismiss());
 
-                    //Code action for the OK button:
-                    Button button_PinCodeOK = customLayout.findViewById(R.id.button_PinCodeOK);
+                //Code action for the OK button:
+                Button button_PinCodeOK = customLayout.findViewById(R.id.button_PinCodeOK);
 
-                    //Code action for the OK button:
-                    button_PinCodeOK.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
+                EditText editText_DialogInput = customLayout.findViewById(R.id.editText_DialogInput);
 
-                            if(!globalClass.gabTagsLoaded.get()){
-                                Toast.makeText(getApplicationContext(), "Await tag loading...", Toast.LENGTH_SHORT).show();
-                                return;
-                            }
+                //Code action for the OK button:
+                button_PinCodeOK.setOnClickListener(view12 -> {
 
-                            EditText editText_DialogInput = customLayout.findViewById(R.id.editText_DialogInput);
-                            String sPinEntered = editText_DialogInput.getText().toString();
+                    if(!globalClass.gabTagsLoaded.get()){
+                        Toast.makeText(getApplicationContext(), "Await tag loading...", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
 
-                            if (sPinEntered.equals(icu.sPin)) {
-                                GlobalClass.gicuCurrentUser = icu;
-                                globalClass.populateApprovedTags();
-                                resetContentSortAndFilterTags();
-                                Toast.makeText(getApplicationContext(), sWelcomeMessage, Toast.LENGTH_SHORT).show();
-                                adConfirmationDialog.dismiss();
-                                closeActivity(getApplicationContext());
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Incorrect pin entered.", Toast.LENGTH_SHORT).show();
-                                adConfirmationDialog.dismiss();
-                            }
+                    String sPinEntered = editText_DialogInput.getText().toString();
 
-                        }
-                    });
+                    if (sPinEntered.equals(icu.sPin)) {
+                        GlobalClass.gicuCurrentUser = icu;
+                        globalClass.populateApprovedTags();
+                        resetContentSortAndFilterTags();
+                        Toast.makeText(getApplicationContext(), sWelcomeMessage, Toast.LENGTH_SHORT).show();
+                        adConfirmationDialog.dismiss();
+                        closeActivity(getApplicationContext());
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Incorrect pin entered.", Toast.LENGTH_SHORT).show();
+                        adConfirmationDialog.dismiss();
+                    }
 
-                    adConfirmationDialog.show();
+                });
 
-                } else {
-                    //If this user record does not require a pin to log-in, merely log-in.
-                    GlobalClass.gicuCurrentUser = icu;
-                    globalClass.populateApprovedTags();
-                    resetContentSortAndFilterTags();
-                    Toast.makeText(getApplicationContext(), sWelcomeMessage, Toast.LENGTH_SHORT).show();
-                    closeActivity(getApplicationContext());
-                }
+                adConfirmationDialog.show();
 
-            } //End if ListView.onItemClick().
+                editText_DialogInput.requestFocus();
+
+            } else {
+                //If this user record does not require a pin to log-in, merely log-in.
+                GlobalClass.gicuCurrentUser = icu;
+                globalClass.populateApprovedTags();
+                resetContentSortAndFilterTags();
+                Toast.makeText(getApplicationContext(), sWelcomeMessage, Toast.LENGTH_SHORT).show();
+                closeActivity(getApplicationContext());
+            }
 
         }); //End ListView.setOnItemClickListener()
 
