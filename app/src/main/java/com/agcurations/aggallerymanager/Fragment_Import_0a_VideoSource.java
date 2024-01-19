635e126d3dd44d9e4da904a035e796abcd5facb3
Dabnoot <William.Chapman970@gmail.com>
2024-01-19 11:56:20 -0500

    Code simplification.

--- app/src/main/java/com/agcurations/aggallerymanager/Fragment_Import_0a_VideoSource.java
+++ app/src/main/java/com/agcurations/aggallerymanager/Fragment_Import_0a_VideoSource.java
@@ -2,6 +2,7 @@ package com.agcurations.aggallerymanager;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -42,6 +43,14 @@ public class Fragment_Import_0a_VideoSource extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_import_0a_video_source, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
     }
 
     @Override
