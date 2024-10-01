883cc41cb48904154b5cb3d974f6e6ac714ff125
Dabnoot <William.Chapman970@gmail.com>
2024-10-01 16:04:01 -0400

    Updated maturity adapter as index was off.

--- app/src/main/java/com/agcurations/aggallerymanager/AdapterMaturityRatings.java
+++ app/src/main/java/com/agcurations/aggallerymanager/AdapterMaturityRatings.java
@@ -20,13 +20,13 @@ public class AdapterMaturityRatings extends ArrayAdapter<String[]> {
     //  You must update tags, catalog items, and user accounts if you wish to modify.
 
     //Adapted from ESRB Ratings
-    public final static int MATURITY_RATING_EC = 0;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
-    public final static int MATURITY_RATING_E = 1;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
-    public final static int MATURITY_RATING_E10 = 2;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
-    public final static int MATURITY_RATING_T = 3;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
-    public final static int MATURITY_RATING_M = 4;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
-    public final static int MATURITY_RATING_AO = 5;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
-    public final static int MATURITY_RATING_RP = 6;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
+    public final static int MATURITY_RATING_RP = 0;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
+    public final static int MATURITY_RATING_EC = 1;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
+    public final static int MATURITY_RATING_E = 2;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
+    public final static int MATURITY_RATING_E10 = 3;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
+    public final static int MATURITY_RATING_T = 4;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
+    public final static int MATURITY_RATING_M = 5;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
+    public final static int MATURITY_RATING_AO = 6;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
     public final static int MATURITY_RATING_IB = 7;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
     public final static int MATURITY_RATING_X = 8;  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
 
@@ -36,13 +36,13 @@ public class AdapterMaturityRatings extends ArrayAdapter<String[]> {
     public final static int MATURITY_RATING_NAME_INDEX = 1;
     public final static int MATURITY_RATING_DESCRIPTION_INDEX = 2;
     public final static String[][] MATURITY_RATINGS = {  //DO NOT CHANGE WITHOUT UPDATING STORED USER DATA.
+            {"RP",  "Rating Pending",           "Titles with the RP rating have not yet been assigned a final ESRB rating."},
             {"EC",  "Early Childhood",          "Suitable for children aged 3 or older; there will be no inappropriate content. E.g., Dora the Explorer, Dragon Tales."},
             {"E",   "Everyone",                 "Suitable for all age groups. The game should not contain any sounds or images likely to scare young children. No bad language should be used. E.g., Just Dance, FIFA.",},
             {"E10+","Everyone 10 and Older",    "Suitable for those aged 10 or above. There could be mild forms of violence, and some scenes might be frightening for children. E.g., Minecraft Dungeons, Plants vs Zombies.",},
             {"T",	"Teen",                     "Suitable for those aged 13 or above. The game could feature more realistic and graphic scenes of violence. E.g., Fortnite, Sims 4.",},
             {"M",	"Mature",                   "Suitable for those aged 17 or above. This rating is used when the violence becomes realistic and would be expected in real life. Bad language, and the use of tobacco, alcohol, or illegal drugs can also be present. E.g., Ark: Survival Evolved, Destiny 2.",},
             {"AO",	"Adults Only",              "Suitable for adults aged 18 or above. The adult classification is used when there are extreme levels of violence and motiveless killing. Glamorization of drugs, gambling, and sexual activity can also be featured. E.g., Grand Theft Auto V, Fallout 4.",},
-            {"RP",  "Rating Pending",           "Titles with the RP rating have not yet been assigned a final ESRB rating."},
             {"IB",  "Implicit Bias",            "Associated with items eschewed by mainstream society, may vary by country, religion, or culture. Implicit Bias is defined as negative associations expressed automatically. May include L.G.B.T.Q.I.A topics in socially-repressive countries. Includes some NFSW content."},
             {"X",  "X-Rated",                   "Associated with items highly eschewed by mainstream society, such as pron, or some topics in certain countries, religions, or cultures. All content should be considered NFSW."},
              //DO NOT CHANGE ORDER OR INDEXING WITHOUT UPDATING STORED USER DATA.
@@ -50,11 +50,10 @@ public class AdapterMaturityRatings extends ArrayAdapter<String[]> {
 
     public static ArrayList<String> GetMaturityStringsForUserAssignment(){
         ArrayList<String> alsShortStrings = new ArrayList<>();
-        for(int i = 0; i < RATINGS_COUNT - 1; i++){
+        for(int i = 0; i < RATINGS_COUNT; i++){
             if(i <= GlobalClass.gicuCurrentUser.iMaturityLevel) {
                 //Don't allow a user to create a user with a maturity rating higher than theirself.
-                if (!(MATURITY_RATINGS[i][MATURITY_RATING_CODE_INDEX].equals(
-                        MATURITY_RATINGS[MATURITY_RATING_RP][MATURITY_RATING_CODE_INDEX]))) {
+                if (i != MATURITY_RATING_RP) {
                     //Don't add "rating pending" to list of short strings. Those are for tags only.
                     String sLine = MATURITY_RATINGS[i][0] + " - " + MATURITY_RATINGS[i][1];
                     alsShortStrings.add(sLine);
@@ -66,7 +65,7 @@ public class AdapterMaturityRatings extends ArrayAdapter<String[]> {
 
     public static ArrayList<String> GetMaturityStringsForTagAssigment(){
         ArrayList<String> alsShortStrings = new ArrayList<>();
-        for(int i = 0; i < RATINGS_COUNT - 1; i++){
+        for(int i = 0; i < RATINGS_COUNT; i++){
             if(i <= GlobalClass.gicuCurrentUser.iMaturityLevel) {
                 //Don't allow a user to create a user with a maturity rating higher than theirself.
                 String sLine = MATURITY_RATINGS[i][0] + " - " + MATURITY_RATINGS[i][1];
