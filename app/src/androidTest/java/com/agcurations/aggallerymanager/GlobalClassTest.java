105d8e2482293bd9b3911e6112604be5fe122955
Dabnoot <William.Chapman970@gmail.com>
2024-10-25 18:25:37 -0400

    Debugging and testing stuff.

--- app/src/androidTest/java/com/agcurations/aggallerymanager/GlobalClassTest.java
+++ app/src/androidTest/java/com/agcurations/aggallerymanager/GlobalClassTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class GlobalClassTest {
 
     @Test
@@ -20,10 +25,11 @@ public class GlobalClassTest {
     }
 
     @Test
-    public void Test_GetRelativePathFromUriString(){
+    public void Test_GetRelativePathFromUriString() throws UnsupportedEncodingException {
         String sDataFolder = "content://com.android.externalstorage.documents/tree/3538-3631%3AArchive%2FAGGalleryManager/document/3538-3631%3AArchive%2FAGGalleryManager";
         String sPath = "content://com.android.externalstorage.documents/tree/3538-3631%3AArchive%2FAGGalleryManager/document/3538-3631%3AArchive%2FAGGalleryManager%2FVideos%2F1%2F10089";
-        String sSuccessfulResult = "1/10089";
+        String sSuccessfulResult = "1%2F10089";
+        GlobalClass.gsFileSeparator = URLEncoder.encode(File.separator, StandardCharsets.UTF_8.toString());
         String sResult = GlobalClass.GetRelativePathFromUriString(sPath, sDataFolder);
         assertEquals(sResult, sSuccessfulResult);
     }
